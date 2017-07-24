6dc8e90938379bc9eacb1ced8c65f2f66e07c267
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-07-23 21:46:58 -0700

    changed routes to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :food_by_lags
-
+    resources :users
+    root to: "users#index"
 end
