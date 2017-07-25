6edec6a8ff608d0996d305024b22ef5d8e51af42
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-07-25 14:46:07 -0700

    Big update, sessions, users update, admin juliana login, password 1234, shipping address 1 and 2, visit count in application controller as well as current_user helper method and authenticate admin(not active on any models)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "/users/signout" => "sessions#signout"
+  	get "/users/:id/edit" => "users#edit"
+  	patch "/users/:id/edit" => "users#update"
     resources :users
     root to: "users#index"
+    post "/users/signin" => "sessions#signin"
+
 end
