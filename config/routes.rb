9d05f85866c6dd9f8d9da98a7d3702547fc0cfa1
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-08-04 21:13:54 -0700

    stripe api skeleton ands seperate views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   	get "/users/:id/edit" => "users#edit"
   	patch "/users/:id/edit" => "users#update"
     resources :users
+    resources :charges
     root to: "users#comingsoon"
     post "/users/signin" => "sessions#signin"
     get "/signout" => "sessions#signout"
