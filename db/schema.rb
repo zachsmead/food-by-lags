5c89c6d09f8abe0e1d7ba1047d76e67470873db1
= <zrsmead@gmail.com>
2017-09-01 21:10:20 -0700

    added images to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819083936) do
+ActiveRecord::Schema.define(version: 20170902040807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170819083936) do
     t.decimal  "cost",         precision: 9, scale: 2
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+    t.string   "image"
   end
 
   create_table "texts", force: :cascade do |t|
