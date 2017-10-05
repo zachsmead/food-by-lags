bc9c81f33333a02fba45b7e14421aaadb588f579
= <zrsmead@gmail.com>
2017-10-04 20:09:34 -0700

    changing application.rb again

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,5 +11,6 @@ module JlfoodByLags
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
