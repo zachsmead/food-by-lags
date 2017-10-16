baccba8b83a369d5f69bc79b4c300dc62aef38d3
James Lagattuta <jimmy.lagattuta@gmail.com>
2017-10-15 18:55:55 -0700

    updated text_approval and comment_approval, fixed bug

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -23,12 +23,13 @@ class OrderMailer < ApplicationMailer
 
   def text_approval(text)
     email = 'foodbylags@gmail.com'
-    @text = text
+    @new_text = text
     @text_text = text.text
     @id = text.id
     @comment_id = text.comment_id
     mail to: email, subject: "Do you approve of this comment"
   end
+
 
 
 end
